95c3e24d9bcb96e225ae52f181d66d3c95d64ab0
Kanut Karomprach <kanut@BALL-61032.local>
2019-08-17 21:47:32 +0700

    create dashboard component and do UI

---
+++ src/app/home/cooking-dashboard/cooking-dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cooking-dashboard',
+  templateUrl: './cooking-dashboard.component.html',
+  styleUrls: ['./cooking-dashboard.component.scss'],
+})
+export class CookingDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
